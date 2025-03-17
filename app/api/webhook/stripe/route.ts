ab9c09ca1aa70f6cf6d28b80e5dab09e3f31fc98
albionn7 <albionnazari@hotmail.com>
2025-03-17 17:50:10 +0100

    fixing stripe webhook file for testing in production

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,32 +1,33 @@
-import Stripe from "stripe";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 import { NextResponse } from "next/server";
 import { createOrder } from "@/lib/actions/order.actions";
 
 export async function POST(request: Request) {
   const body = await request.text();
-
   const sig = request.headers.get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
   let event;
 
   try {
-    event = Stripe.webhooks.constructEvent(body, sig, endpointSecret);
-  } catch (err) {
-    return NextResponse.json({ message: "Webhook error", error: err });
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  } catch (err: any) {
+    console.error("⚠️  Webhook signature verification failed.", err.message);
+    return new Response(`Webhook error: ${err.message}`, { status: 400 });
   }
 
   // Get the ID and type
   const eventType = event.type;
 
-  // CREATE
+  // CREATE ORDER
   if (eventType === "checkout.session.completed") {
-    const { id, amount_total, metadata } = event.data.object;
+    const { id, amount_total } = event.data.object;
+    const metadata = event.data.object.metadata || {}; // Ensure metadata is always an object
 
     const order = {
       stripeId: id,
-      eventId: metadata?.eventId || "",
-      buyerId: metadata?.buyerId || "",
+      eventId: metadata.eventId || "",
+      buyerId: metadata.buyerId || "",
       totalAmount: amount_total ? (amount_total / 100).toString() : "0",
       createdAt: new Date(),
     };
